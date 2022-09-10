e1ac15e08e0d16cb39f9c2d1f3101f446bbf82bf
JBrendon15 <110281109+JBrendon15@users.noreply.github.com>
2022-09-10 04:31:41 -0500

    added the if conditions into a function so i the page can update the currentMoment real time with set interval

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,12 +1,11 @@
 let currentDay = $('#currentDay');
 currentDay.text(moment().format('dddd, MMMM Do'));
-let timeNow = $('#timeNow');
+
 
 
 let scheduleTime = $('#schedule-time');
 let format = 'HH:mm:ss';
 let currentMoment = moment();
-
 let nineOclock = moment('09:00:00', format);
 let tenOclock = moment('10:00:00', format);
 let elevenOclock = moment('11:00:00', format);
@@ -19,81 +18,92 @@ let fiveOclock = moment('17:00:00', format);
 let sixOclock = moment('18:00:00', format);
 let dayBegins = moment('00:00:00', format);
 
+
+
 function captureTime(){
     setInterval(function(){
+        checkTime();
         currentMoment = moment();
+        // code below was for testing
+        // currentMoment.add(59, 'm')
         timeNow.text(currentMoment);
+        
         
     }, 1000);
     
 }
-if(currentMoment.isBetween(dayBegins, nineOclock)){
-    $('#nineAM').addClass('future');
-    $('#tenAM').addClass('future');
-    $('#elevenAM').addClass('future');
-    $('#twelvePM').addClass('future');
-    $('#onePM').addClass('future');
-    $('#twoPM').addClass('future');
-    $('#threePM').addClass('future');
-    $('#fourPM').addClass('future');
-    $('#fivePM').addClass('future');
-}
 
+function checkTime(){
+    if(currentMoment.isSameOrAfter(dayBegins)){
+        $('#nineAM').addClass('future');
+        $('#tenAM').addClass('future');
+        $('#elevenAM').addClass('future');
+        $('#twelvePM').addClass('future');
+        $('#onePM').addClass('future');
+        $('#twoPM').addClass('future');
+        $('#threePM').addClass('future');
+        $('#fourPM').addClass('future');
+        $('#fivePM').addClass('future');
+    }
+    if(currentMoment.isSameOrAfter(nineOclock)){
+        $('#nineAM').removeClass('future');
+        $('#nineAM').addClass('present');       
+    }
+    if(currentMoment.isSameOrAfter(tenOclock)){
+        $('#tenAM').removeClass('future');
+        $('#tenAM').addClass('present');       
+        $('#nineAM').removeClass('present');
+        $('#nineAM').addClass('past');
+    }
+    if(currentMoment.isSameOrAfter(elevenOclock)){
+        $('#elevenAM').removeClass('future');
+        $('#elevenAM').addClass('present');       
+        $('#tenAM').removeClass('present');
+        $('#tenAM').addClass('past');       
+    }
+    if(currentMoment.isSameOrAfter(twelveOclock)){
+        $('#twelvePM').removeClass('future');
+        $('#twelvePM').addClass('present');       
+        $('#elevenAM').removeClass('present');
+        $('#elevenAM').addClass('past');       
+    }
+    if(currentMoment.isSameOrAfter(oneOclock)){
+        $('#onePM').removeClass('future');
+        $('#onePM').addClass('present');       
+        $('#twelvePM').removeClass('present');
+        $('#twelvePM').addClass('past');       
+    }
+    if(currentMoment.isSameOrAfter(twoOclock)){
+        $('#twoPM').removeClass('future');
+        $('#twoPM').addClass('present');       
+        $('#onePM').removeClass('present');
+        $('#onePM').addClass('past');       
+    }
+    if(currentMoment.isSameOrAfter(threeOclock)){
+        $('#threePM').removeClass('future');
+        $('#threePM').addClass('present');       
+        $('#twoPM').removeClass('present');
+        $('#twoPM').addClass('past');       
+    }
+    if(currentMoment.isSameOrAfter(fourOclock)){
+        $('#fourPM').removeClass('future');
+        $('#fourPM').addClass('present');       
+        $('#threePM').removeClass('present');
+        $('#threePM').addClass('past');       
+    }
+    if(currentMoment.isSameOrAfter(fiveOclock)){
+        $('#fivePM').removeClass('future');
+        $('#fivePM').addClass('present');       
+        $('#fourPM').removeClass('present');
+        $('#fourPM').addClass('past');       
+    }
+    if(currentMoment.isSameOrAfter(sixOclock)){
+        $('#fivePM').removeClass('present');
+        $('#fivePM').addClass('past');       
+        
+    }
 
-if(currentMoment.isBetween(nineOclock, tenOclock)){
-    $('#nineAM').removeClass('future');
-    $('#nineAM').addClass('present');       
 }
-if(currentMoment.isBetween(tenOclock, elevenOclock)){
-    $('#tenAM').removeClass('future');
-    $('#tenAM').addClass('present');       
-    $('#nineAM').removeClass('present');
-    $('#nineAM').addClass('past');
-           
-}
-if(currentMoment.isBetween(elevenOclock, twelveOclock)){
-    $('#elevenAM').removeClass('future');
-    $('#elevenAM').addClass('present');       
-    $('#tenAM').removeClass('present');
-    $('#tenAM').addClass('past');       
-}
-if(currentMoment.isBetween(twelveOclock, oneOclock)){
-    $('#twelvePM').removeClass('future');
-    $('#twelvePM').addClass('present');       
-    $('#elevenAM').removeClass('present');
-    $('#elevenAM').addClass('past');       
-}
-if(currentMoment.isBetween(oneOclock, twoOclock)){
-    $('#onePM').removeClass('future');
-    $('#onePM').addClass('present');       
-    $('#twelvePM').removeClass('present');
-    $('#twelvePM').addClass('past');       
-}
-if(currentMoment.isBetween(twoOclock, threeOclock)){
-    $('#twoPM').removeClass('future');
-    $('#twoPM').addClass('present');       
-    $('#onePM').removeClass('present');
-    $('#onePM').addClass('past');       
-}
-if(currentMoment.isBetween(threeOclock, fourOclock)){
-    $('#threePM').removeClass('future');
-    $('#threePM').addClass('present');       
-    $('#twoPM').removeClass('present');
-    $('#twoPM').addClass('past');       
-}
-if(currentMoment.isBetween(fourOclock, fiveOclock)){
-    $('#fourPM').removeClass('future');
-    $('#fourPM').addClass('present');       
-    $('#threePM').removeClass('present');
-    $('#threePM').addClass('past');       
-}
-if(currentMoment.isBetween(fiveOclock, sixOclock)){
-    $('#fivePM').removeClass('future');
-    $('#fivePM').addClass('present');       
-    $('#fourPM').removeClass('present');
-    $('#fourPM').addClass('past');       
-}
-
 $(document).ready(captureTime)
 
 
@@ -122,10 +132,10 @@ $(document).ready(captureTime)
 
 
 
-// let prevMoment = moment().hour(6);
-// let nextMoment = moment().hour(7);
+// let prevMoment = moment().hour(hour_before);
+// let nextMoment = moment().hour(hour_after);
 
-// console.log(currentMoment.isBetween(prevMoment, nextMoment));
+// console.log(currentMoment.isSameOrAfter(prevMoment, nextMoment));
 
 // let currentTime = moment().format(format);
 // console.log(currentTime);
